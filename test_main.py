73ceb5e1a519e5b21c9a7f2ef3b1d006463ea10f
Krasnokutskyi Valentyn <kras.vvk@gmail.com>
2024-02-01 13:14:35 -0500

    Commit new_branch commit add aa, aaa

--- test_main.py
+++ test_main.py
@@ -1,3 +1,8 @@
 """Branch main first commit"""
 a = 1
+aa = 2
+aaa = 3
 """Branch new_branch second commit"""
+"""first coment branch test_2"""
+"""second coment branch test_2"""
+"""third coment branch test_2"""
